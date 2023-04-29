e3fd8e2f57eb1ee502134d19b0d9c0b738d79e05
Team Agora <132120521+TeamAgora@users.noreply.github.com>
2023-04-29 10:19:56 +0530

    TeamAgora

--- AnonX/plugins/start.py
+++ AnonX/plugins/start.py
@@ -43,7 +43,7 @@ async def start_comm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            await message.reply_sticker("CAACAgUAAx0CZzVdqgACAlxkQjF_67sQLBbvw5L0pM571iEvyQACdgcAAtdJEFZ5OC4clfQidS8E")
+            await message.reply_sticker("CAACAgUAAx0CaTIO5wACATdkTKFboKRqdIDBfuU3NlorLYvRCQACvgsAAqqKYVYIKYyquq19ES8E")
             return await message.reply_photo(
                        photo=config.START_IMG_URL,
                        caption=_["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard
@@ -88,7 +88,7 @@ async def start_comm(client, message: Message, _):
                     details = stats.get(vidid)
                     title = (details["title"][:35]).title()
                     if vidid == "telegram":
-                        msg += f"🔗[ᴛᴇʟᴇɢʀᴀᴍ ᴍᴇᴅɪᴀ](https://t.me/AGORAWORLD) ** ᴩʟᴀʏᴇᴅ {count} ᴛɪᴍᴇs**\n\n"
+                        msg += f"🔗[ᴛᴇʟᴇɢʀᴀᴍ ᴍᴇᴅɪᴀ](https://t.me/teamagora) ** ᴩʟᴀʏᴇᴅ {count} ᴛɪᴍᴇs**\n\n"
                     else:
                         msg += f"🔗 [{title}](https://www.youtube.com/watch?v={vidid}) ** played {count} times**\n\n"
                 msg = _["ustats_2"].format(tot, tota, limit) + msg
@@ -173,7 +173,7 @@ async def start_comm(client, message: Message, _):
                             text="• ʏᴏᴜᴛᴜʙᴇ •", url=f"{link}"
                         ),
                         InlineKeyboardButton(
-                            text="• sᴜᴩᴩᴏʀᴛ •", url="https://t.me/AGORA_BOTS"
+                            text="• sᴜᴩᴩᴏʀᴛ •", url="https://t.me/teamagora"
                         ),
                     ],
                 ]
@@ -202,7 +202,7 @@ async def start_comm(client, message: Message, _):
         out = private_panel(_, app.username, OWNER)
         if config.START_IMG_URL:
             try:
-                await message.reply_sticker("CCAACAgUAAx0CZzVdqgACAlxkQjF_67sQLBbvw5L0pM571iEvyQACdgcAAtdJEFZ5OC4clfQidS8E")
+                await message.reply_sticker("CAACAgUAAx0CaTIO5wACATdkTKFboKRqdIDBfuU3NlorLYvRCQACvgsAAqqKYVYIKYyquq19ES8E")
                 await message.reply_photo(
                     photo=config.START_IMG_URL,
                     caption=_["start_2"].format(
