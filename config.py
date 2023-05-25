2e172f725db8ac98ab62cacdfb49743cc775f612
Team Agora <132120521+TeamAgora@users.noreply.github.com>
2023-05-25 16:52:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "6181817811").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/TeamAgora/Music")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/TeamAgora/agoraxmusic")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
